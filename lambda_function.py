731cf8e1635c053b93376917081a137f9694b7ed
Ruban Raj David <Ruban.David@Wlt.com>
2021-08-11 12:27:50 +0530

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,12 @@
+import boto3
+import json
+        
+ec2 = boto3.client('ec2')
+def lambda_handler(event, context):
+    azs = ec2.describe_availability_zones()
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            azs
+        })
+    }
